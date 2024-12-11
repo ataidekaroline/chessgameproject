c96e5d3f0d7819286368143aeaa7af91094e46f8
Luana Ataide <ataideacademic@gmail.com>
2024-12-11 15:55:42 -0300

    Projeto completo - finalizado!

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,28 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using tabuleiro;
+﻿using xadrez_console.tabuleiro;
 
-namespace xadrez_console.tabuleiro
+namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
-        public Posicao posicao {  get; set; }
-        public Cor cor { get; protected set; }
-        public  int qteMovimentos { get; protected set; }
-        public Tabuleiro tab { get ; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
-            this.cor = cor;
+            this.posicao = null;
             this.tab = tab;
+            this.cor = cor;
             this.qteMovimentos = 0;
         }
 
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
 
+        public void decrementarQteMovimentos()
+        {
+            qteMovimentos--;
+        }
 
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool movimentoPossivel(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
+
         public int linha { get; set; }
         public int coluna { get; set; }
 
         public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public void definirValores(int linha, int coluna)
         {
             this.linha = linha;
             this.coluna = coluna;
@@ -23,7 +24,5 @@ namespace tabuleiro
                 + ", "
                 + coluna;
         }
-
-
     }
 }
